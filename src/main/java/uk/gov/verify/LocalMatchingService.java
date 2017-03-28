799ea696484aedd6a43e4ef98971b9ba5d8a4c36
Hristo Georgiev <hristo.georgiev@digital.cabinet-office.gov.uk>
2017-03-28 15:08:13 +0100

    TT-298: Printing out request body for debugging.
    Authors: @richardtowers @georgievh

--- src/main/java/uk/gov/verify/LocalMatchingService.java
+++ src/main/java/uk/gov/verify/LocalMatchingService.java
@@ -12,16 +12,20 @@ public class LocalMatchingService {
     public static final int DEFAULT_PORT = 50130;
 
     public static void main(String[] args) {
-        Spark.port(parseInt(getenv().getOrDefault("LMS_PORT", valueOf(DEFAULT_PORT))));
+        String port = getenv().getOrDefault("LMS_PORT", valueOf(DEFAULT_PORT));
+        System.out.println("Listening on port: " + port);
+        Spark.port(parseInt(port));
         addRoutes();
     }
 
     private static void addRoutes() {
         post("/local-matching/match", (req, res) -> {
+            System.out.println(req.body());
             res.type("application/json");
             return "{\"result\":\"match\"}";
         });
         post("/local-matching/create-user", (req, res) -> {
+            System.out.println(req.body());
             res.type("application/json");
             return "{\"result\":\"success\"}";
         });
